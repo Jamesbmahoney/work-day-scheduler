3d9677ba4cb9f235a343fe2de84bead19ac93fd8
James Barry <jbm03003@gmail.com>
2021-12-25 18:55:54 -0500

    added style to buttons, functionality to color change on time

--- Develop/script.js
+++ Develop/script.js
@@ -39,3 +39,38 @@ function dateTime() {
 }
 
 dateTime();
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+var time = new Date();
+var hour = time.getHours();
+
+var currentHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+for (var i = 0; i < currentHour.length; i++) {
+    console.log(currentHour[i]);
+    if (hour < currentHour[i]) {
+        $("#" + currentHour[i])
+            .removeClass("present")
+            .addClass("future");
+    } else if (hour > currentHour[i]) {
+        console.log(hour);
+        $("#" + currentHour[i])
+            .removeClass("present")
+            .addClass("past");
+    } else {
+        $("#" + currentHour[i])
+            .addClass("present");
+    }
+}
+
+
+
+
